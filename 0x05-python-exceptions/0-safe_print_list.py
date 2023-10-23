6afa01da2172bf03167f73f6555ffe74e448c2a1
Joseph-Oje <josephoje247@gmail.com>
2023-10-23 10:02:00 -0700

    task 0 and task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(0, x):
+    cout = 0
+    for i in my_list:
         try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except:
+            if cout == x:
+                break
+            print("{:d}".format(i), end="")
+            cout += 1
+        except Exception as e:
             pass
     print()
-    return count
+    return cout
