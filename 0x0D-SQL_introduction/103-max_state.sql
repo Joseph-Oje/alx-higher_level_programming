19ac68d6bdd8460bdd3ce275bb4a72cb1e95dae0
Joseph-Oje <josephoje247@gmail.com>
2023-12-13 14:04:33 -0800

    SQL introduction

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,3 @@
--- Script that displays the max temperature of each state
--- Query to display the max temperature of each state
-SELECT state, MAX(value) AS max_temp
-FROM temperatures
-GROUP BY state
-LIMIT 3;
+-- This would List max value by state
+-- Execute: cat 103-max_state.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
